5f8c4f94ebf63e17bf2793bf4a7a80536ff46683
Rhuan <rhuan.projetos@gmail.comgit config --global user.email rhuan.projetos@gmail.com>
2025-03-10 13:27:53 +0100

    Adicionando comentários ao código de fatorial

--- fatorial.py
+++ fatorial.py
@@ -1,8 +1,13 @@
+# Função para calcular o fatorial de um número - em python
+
 def fatorial(n):
+    # Se n for zero, o fatorial é 1
     if n == 0:
         return 1
     else:
         return n * fatorial(n-1)
 
+# Solicita ao usuário um número e calcula o fatorial
 numero = int(input("Digite um número para calcular o fatorial: "))
 print(f"O fatorial de {numero} é {fatorial(numero)}")
+# f dentro do print é para deixar tudo como string
